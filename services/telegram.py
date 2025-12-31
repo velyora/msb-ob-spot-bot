0f5769fe795cd18f05fa0d89421c807c20667267
Velyaora <jinalk620@gmail.com>
2025-12-31 13:53:57 +0300

    Add function to send messages via Telegram API
    Signed-off-by: Velyaora <jinalk620@gmail.com>

---
+++ services/telegram.py
@@ -0,0 +1,12 @@
+import requests
+
+def send_message(token, chat_id, text):
+    url = f"https://api.telegram.org/bot{token}/sendMessage"
+    payload = {
+        "chat_id": chat_id,
+        "text": text,
+        "parse_mode": "HTML",
+        "disable_web_page_preview": True
+    }
+    r = requests.post(url, json=payload, timeout=15)
+    r.raise_for_status()
